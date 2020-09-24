61ee861ef7cbb990cc199b1324c7684b1c566f3d
Helmuth Risch <helmuthfilho001@gmail.com>
2020-09-24 20:56:11 -0300

    testing

--- Panda3D/AR_Test/main.py
+++ Panda3D/AR_Test/main.py
@@ -11,9 +11,10 @@ class ARtest(ShowBase):
         self.accept('1', self.detachObjetct)
 
         #Windows WebCam
-        option = WebcamVideo.getOption(1)  # 0 here is default webcam, 1 would be second cam etc.
+        option = WebcamVideo.getOption(0)  # 0 here is default webcam, 1 would be second cam etc.
         self.tex = MovieTexture(option)
-        self.tex.setTexturesPower2(0)
+        #self.tex.setTexturesPower2(0)
+        self.tex.setKeepRamImage(True)
 
         print("WebCamVideo based texture infos: -> {0}".format(self.tex))
 
