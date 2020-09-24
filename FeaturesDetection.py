98512ebeefe69a89b0a4f029348dc94a4a859922
Lucas Laheras <28765277+LucasLaheras@users.noreply.github.com>
2020-09-24 17:52:06 -0300

    Update FeaturesDetection.py
    renamed variables

--- FeaturesDetection.py
+++ FeaturesDetection.py
@@ -26,16 +26,16 @@ def angle_check(img, photo_directory, MIN_MATCH_COUNT=10):
         #boundbox_values = boundbox_values.split(" ")
         #boundbox_values = list(map(float, boundbox_values[1:]))
 
-        img1 = cv.imread(file, 0)
-        #img1 = img1.crop[boundbox_values[0]:boundbox_values[1], boundbox_values[2]:boundbox_values[3]]
-        #img1 = cv.resize(img1, size_image)
+        img_angle = cv.imread(file, 0)
+        #img_angle = img_angle.crop[boundbox_values[0]:boundbox_values[1], boundbox_values[2]:boundbox_values[3]]
+        #img_angle = cv.resize(img_angle, size_image)
 
         # Initiate SIFT detector
         sift = cv.SIFT_create()
 
         # find the keypoints and descriptors with SIFT
-        kp1, des1 = sift.detectAndCompute(img, None)
-        kp2, des2 = sift.detectAndCompute(img1, None)
+        kp1, des1 = sift.detectAndCompute(img_angle, None)
+        kp2, des2 = sift.detectAndCompute(img, None)
 
         FLANN_INDEX_KDTREE = 1
         index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
@@ -68,7 +68,7 @@ if len(good) > MIN_MATCH_COUNT:
     h, w = img1.shape
     pts = np.float32([[0, 0], [0, h-1], [w-1, h-1], [w-1, 0]]).reshape(-1, 1, 2)
     dst = cv.perspectiveTransform(pts, M)
-    img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+    img_angle = cv.polylines(img_angle, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
 else:
     print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
     matchesMask = None
@@ -77,7 +77,7 @@ draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
                    singlePointColor=None,
                    matchesMask=matchesMask,  # draw only inliers
                    flags=2)
-img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+img3 = cv.drawMatches(img1, kp1, img_angle, kp2, good, None, **draw_params)
 #plt.imshow(cv.cvtColor(img3, cv.COLOR_BGR2RGB)),plt.show()
 plt.imshow(img3, 'gray'), plt.show()
 '''''
@@ -98,14 +98,14 @@ if __name__ == '__main__':
     #file_name = 'DS4/PlayStation2_1.JPG'
     print(file_name)
 
-    img2 = cv.imread(file_name, 0)
-    #img2 = cv.resize(img2, size_image)
+    img_angle = cv.imread(file_name, 0)
+    #img_angle = cv.resize(img_angle, size_image)
     # Initiate SIFT detector
     sift = cv.SIFT_create()
 
     # find the keypoints and descriptors with SIFT
-    kp1, des1 = sift.detectAndCompute(img1, None)
-    kp2, des2 = sift.detectAndCompute(img2, None)
+    kp1, des1 = sift.detectAndCompute(img_angle, None)
+    kp2, des2 = sift.detectAndCompute(img1, None)
 
     FLANN_INDEX_KDTREE = 1
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
@@ -123,11 +123,11 @@ if __name__ == '__main__':
         dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
         M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
         matchesMask = mask.ravel().tolist()
-        print(img1.shape)
-        h, w = img1.shape
+        print(img_angle.shape)
+        h, w = img_angle.shape
         pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
         dst = cv.perspectiveTransform(pts, M)
-        img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+        img1 = cv.polylines(img1, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
     else:
         print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
         matchesMask = None
@@ -136,7 +136,7 @@ if __name__ == '__main__':
                        singlePointColor=None,
                        matchesMask=matchesMask,  # draw only inliers
                        flags=2)
-    img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+    img3 = cv.drawMatches(img_angle, kp1, img1, kp2, good, None, **draw_params)
     #plt.imshow(cv.cvtColor(img3, cv.COLOR_BGR2RGB)),plt.show()
     plt.imshow(img3, 'gray'), plt.show()
 
