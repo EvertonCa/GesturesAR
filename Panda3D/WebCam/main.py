4e559bff9aba92e26ab28696ca0284ee0897da26
Helmuth Risch <helmuthfilho001@gmail.com>
2020-09-24 23:09:39 -0300

    i've told that would be my last commit tonight, but i was wrong

--- Panda3D/WebCam/main.py
+++ Panda3D/WebCam/main.py
@@ -1,11 +1,14 @@
+from click._compat import raw_input
 from panda3d.core import loadPrcFileData
 from panda3d.vision import WebcamVideo
 from panda3d.core import CardMaker, Point2, MovieTexture
 from direct.showbase.ShowBase import ShowBase
 import sys
 
-loadPrcFileData("", "textures-power-2 none")
-loadPrcFileData('', 'client-sleep 0.001')
+loadPrcFileData("", "textures-power-2 none") #the webcam feed can not be made into a power of two texture
+loadPrcFileData("", "show-frame-rate-meter 1") #show fps
+loadPrcFileData("", "sync-video 0") #turn off v-sync
+loadPrcFileData("", "auto-flip 1") #usualy the drawn texture lags a bit behind the calculted positions. this is a try to reduce the lag.
 
 
 class WebCamTest(ShowBase):
@@ -13,11 +16,15 @@ class WebCamTest(ShowBase):
     def __init__(self):
         ShowBase.__init__(self)
         # using WebcamVideo
-        self.printWebOptions()
+        for x, option in enumerate(WebcamVideo.getOptions()):
+            print(option, x)
+        print("Choose webcam and resolution by index:")
 
-        option = WebcamVideo.getOption(0)
+        # Starting Webcam and placing the feed on the background card
+        option = WebcamVideo.getOption(int(raw_input()))
+        #option = WebcamVideo.getOption(0)
         videoTexture = MovieTexture(option)
-        videoTexture.setKeepRamImage(False)
+        videoTexture.setKeepRamImage(True)
         print("WebCamVideo based texture infos: -> {0}".format(videoTexture))
 
         cm = CardMaker("card")
@@ -30,9 +37,6 @@ class WebCamTest(ShowBase):
 
         self.accept('escape', lambda: sys.exit())
 
-    def printWebOptions(self):
-        for i in WebcamVideo.getOptions():
-            print(i)
 
 app = WebCamTest()
 app.run()
