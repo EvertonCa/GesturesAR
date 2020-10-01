294b90fc8e36d0845b88918e7a6d75efe8715d4e
EvertonCa <eve.023@hotmail.com>
2020-10-01 05:36:55 -0300

    - webcam working on macOS

--- SharedMemory/webcam.cpp
+++ SharedMemory/webcam.cpp
@@ -3,32 +3,18 @@
 //
 
 #include "opencv2/opencv.hpp"
-#include "iostream"
 
 int main(int, char**) {
-    // open the first webcam plugged in the computer
-    cv::VideoCapture camera(0);
-    if (!camera.isOpened()) {
-        std::cerr << "ERROR: Could not open camera" << std::endl;
-        return 1;
-    }
+    cv::namedWindow("Video", cv::WINDOW_AUTOSIZE);
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened())
+        return -1;
 
-    // create a window to display the images from the webcam
-    cv::namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
-
-    // this will contain the image from the webcam
     cv::Mat frame;
-
-    // capture the next frame from the webcam
-    camera >> frame;
-
-    // display the frame until you press a key
-    while (1) {
-        // show the image on the window
-        cv::imshow("Webcam", frame);
-        // wait (10ms) for a key to be pressed
-        if (cv::waitKey(10) >= 0)
-            break;
+    for ( ; ; ) {
+        cap >> frame;
+        cv::imshow("Video", frame);
+        if (cv::waitKey(33) >= 0) break;
     }
     return 0;
 }
