276ffd08bd1772c7da8ad9f6db50ef2540a6cd98
EvertonCa <eve.023@hotmail.com>
2020-10-01 05:06:36 -0300

    - updated to C++

---
+++ SharedMemory/destroy_shmem.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Everton Cardoso on 01/10/20.
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "shared_memory.h"
+
+int main(int argc, char *argv[]) {
+    if (argc != 1) {
+        printf("usage - %s //no args", argv[0]);
+        return -1;
+    }
+
+    if (destroy_memory_block(FILENAME)) {
+        printf("Destroyed block: %s\n", FILENAME);
+    } else {
+        printf("Could not destroy block: %s\n", FILENAME);
+    }
+
+    return 0;
+}
+
---
+++ SharedMemory/shared_memory.cpp
@@ -0,0 +1,61 @@
+//
+// Created by Everton Cardoso on 01/10/20.
+//
+
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include "shared_memory.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define IPC_RESULT_ERROR (-1)
+
+static int get_shared_block(char *filename, int size) {
+    key_t key;
+
+    // request a key
+    // the key is linked to a filename, so that other programs can access it
+    key = ftok(filename, 0);
+    if (key == IPC_RESULT_ERROR) {
+        return IPC_RESULT_ERROR;
+    }
+
+    // get shared block --- create it if it doesn't exist
+    return shmget(key, size, 0644 | IPC_CREAT);
+}
+
+char * attach_memory_block(char *filename, int size) {
+    int shared_block_id = get_shared_block(filename, size);
+    char *result;
+
+    if (shared_block_id == IPC_RESULT_ERROR) {
+        return NULL;
+    }
+
+    //map the shared block int this process's memory and give me a pointer to it
+    result = (char*) shmat(shared_block_id, NULL, 0);
+    if (result == (char *)IPC_RESULT_ERROR) {
+        return NULL;
+    }
+
+    return result;
+}
+
+bool detach_memory_block(char *block) {
+    return (shmdt(block) != IPC_RESULT_ERROR);
+}
+
+bool destroy_memory_block(char *filename) {
+    int shared_block_id = get_shared_block(filename, 0);
+
+    if (shared_block_id == IPC_RESULT_ERROR) {
+        return NULL;
+    }
+
+    return (shmctl(shared_block_id, IPC_RMID, NULL) != IPC_RESULT_ERROR);
+}
+
