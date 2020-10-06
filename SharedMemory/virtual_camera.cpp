6b8fad671abceba48f09031b78cac64c55175f57
EvertonCa <eve.023@hotmail.com>
2020-10-06 02:14:39 -0300

    - testing virtual camera

---
+++ SharedMemory/virtual_camera.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Everton Cardoso on 06/10/20.
+//
+
+#include "opencv2/opencv.hpp"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+
+int main() {
+    cv::namedWindow("Test Cam", cv::WINDOW_AUTOSIZE);
+    cv::VideoCapture cap(0);
+    if (!cap.isOpened())
+        return -1;
+
+    cv::Mat frame;
+
+    while (true) {
+
+        cap >> frame;
+
+        cv::imshow("Share Cam", frame);
+
+        // runs until ESC key is pressed
+        if (cv::waitKey(1000/30) == 27) {
+            std::cout << "quit" << std::endl;
+            break;
+        }
+    }
+
+    return 0;
+}
