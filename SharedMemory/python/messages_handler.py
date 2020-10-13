0468acdcb1f9f96e6d0c0f97d2935d5bbf345f8f
EvertonCa <eve.023@hotmail.com>
2020-10-13 16:29:06 -0300

    - reverted to separated programs for getting messages from modules - updated messages_handler.py to reflect previous change

--- SharedMemory/python/messages_handler.py
+++ SharedMemory/python/messages_handler.py
@@ -1,17 +1,39 @@
 from subprocess import Popen, PIPE
+import threading
 
-if __name__ == "__main__":
-    p = Popen(['../cmake-build-debug/MessagesHandler'], shell=True, stdout=PIPE, stdin=PIPE)
+
+def get_messages(module):
+    if module == "YOLO":
+        p = Popen(['../cmake-build-debug/GetMessageYolo'], shell=True, stdout=PIPE, stdin=PIPE)
+    elif module == "SLAM":
+        p = Popen(['../cmake-build-debug/GetMessageSlam'], shell=True, stdout=PIPE, stdin=PIPE)
+    else:
+        p = Popen(['../cmake-build-debug/GetMessageHands'], shell=True, stdout=PIPE, stdin=PIPE)
     while True:
         #value = bytes(value, 'UTF-8')  # Needed in Python 3.
         #p.stdin.write(value.encode('utf_8'))
         #p.stdin.flush()
         result = p.stdout.readline().strip().decode()
         print(result)
+        print(module)
 
-        if result[:4] == "YOLO":
-            continue
-        elif result[:4] == "SLAM":
-            continue
-        else:
-            continue
+
+if __name__ == "__main__":
+    # creating thread
+    yolo_thread = threading.Thread(target=get_messages, args=("YOLO",))
+    slam_thread = threading.Thread(target=get_messages, args=("SLAM",))
+    hands_thread = threading.Thread(target=get_messages, args=("HANDS",))
+
+    # starting yolo thread
+    yolo_thread.start()
+    # starting slam thread
+    slam_thread.start()
+    # starting hands thread
+    hands_thread.start()
+
+    # wait until yolo thread is completely executed
+    yolo_thread.join()
+    # wait until slam thread is completely executed
+    slam_thread.join()
+    # wait until hands thread is completely executed
+    hands_thread.join()
---
+++ SharedMemory/src/get_message_hands.cpp
@@ -0,0 +1,56 @@
+//
+// Created by Everton Cardoso on 13/10/20.
+//
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <semaphore.h>
+#include <fcntl.h>
+
+#include "../includes/shared_memory.h"
+
+int main() {
+
+    sem_unlink(HANDS_SEM_MESSAGE_PRODUCER_FNAME);
+    sem_unlink(HANDS_SEM_MESSAGE_CONSUMER_FNAME);
+
+    // setup some semaphores
+    sem_t *sem_prod = sem_open(HANDS_SEM_MESSAGE_PRODUCER_FNAME, O_CREAT, 0660, 0);
+    if (sem_prod == SEM_FAILED) {
+        perror("sem_open/handscamproducer");
+        exit(EXIT_FAILURE);
+    }
+
+    sem_t *sem_cons = sem_open(HANDS_SEM_MESSAGE_CONSUMER_FNAME, O_CREAT, 0660, 1);
+    if (sem_cons == SEM_FAILED) {
+        perror("sem_open/handscamconsumer");
+        exit(EXIT_FAILURE);
+    }
+
+    // grab the shared memory block
+    char *block = attach_memory_block(FILENAME_MESSAGE_HANDS, MESSAGE_BLOCK_SIZE);
+    if (block == NULL) {
+        printf("ERROR: coundn't get block\n");
+        return -1;
+    }
+
+    while (true) {
+        sem_wait(sem_prod);
+        if (strlen(block) > 0) {
+            printf("\"%s\"\n", block);
+            bool done = (strcmp(block, "quit") == 0);
+            block[0] = 0;
+            if (done) {
+                break;
+            }
+        }
+        sem_post(sem_cons);
+    }
+
+    sem_close(sem_prod);
+    sem_close(sem_cons);
+    detach_memory_block(block);
+
+    return 0;
+}
