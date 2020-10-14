1bdbcf79952c3a34a48809809d611e34c746c38c
Helmuth Risch <helmuthfilho001@gmail.com>
2020-10-13 22:43:40 -0300

    add gan nodes tracking

--- Panda3D/AR_Test/main.py
+++ Panda3D/AR_Test/main.py
@@ -3,7 +3,7 @@ from direct.actor.Actor import Actor
 from direct.gui.OnscreenText import OnscreenText
 from panda3d.core import loadPrcFileData, CardMaker, MovieTexture, Filename, Point2, TextNode, CollisionTraverser, \
     CollisionHandlerQueue, CollisionHandlerPusher, CollisionNode, CollisionCapsule, BitMask32, LVecBase3f, LQuaternion, \
-    CollisionSphere
+    CollisionSphere, NodePath
 from direct.showbase.ShowBase import ShowBase
 from panda3d.vision import WebcamVideo, ARToolKit
 from direct.task import Task
@@ -18,12 +18,34 @@ loadPrcFileData("", "auto-flip 1") #usualy the drawn texture lags a bit behind t
 positionArray = []
 globalCounter = 0
 
-ganPositionArray = []
+temp_ganPositionArray = []
+ganPositionArray0 = []
+ganPositionArray1 = []
+ganPositionArray2 = []
+ganPositionArray3 = []
+ganPositionArray4 = []
+ganPositionArray5 = []
+ganPositionArray6 = []
+ganPositionArray7 = []
+ganPositionArray8 = []
+ganPositionArray9 = []
+ganPositionArray10 = []
+ganPositionArray11 = []
+ganPositionArray12 = []
+ganPositionArray13 = []
+ganPositionArray14 = []
+ganPositionArray15 = []
+ganPositionArray16 = []
+ganPositionArray17 = []
+ganPositionArray18 = []
+ganPositionArray19 = []
+ganPositionArray20 = []
+
 globalGanCounter = 0
 actualGanJoint = 0
 
 def genLabelText(text, i, self):
-    return OnscreenText(text=text, parent=self.a2dTopLeft, scale=.06,
+    return OnscreenText(text=text, parent=self.a2dTopLeft, scale=.04,
                         pos=(0.06, -.08 * i), fg=(1, 1, 1, 1),
                         shadow=(0, 0, 0, .5), align=TextNode.ALeft)
 
@@ -114,6 +136,7 @@ class ARtest(ShowBase):
         sleep(1)  # some webcams are quite slow to start up so we add some safety
         self.taskMgr.add(self.updatePatterns, "update-patterns")
 
+
     def addObject(self):
         self.axis = self.loader.loadModel("models/ball")
         self.axis.reparentTo(self.render)
@@ -156,17 +179,80 @@ class ARtest(ShowBase):
 
     def setGanNodesPosition(self, task):
         global globalGanCounter
-        global ganPositionArray
-        global actualGanJoint
+        global temp_ganPositionArray
+        global ganPositionArray0
+        global ganPositionArray1
+        global ganPositionArray2
+        global ganPositionArray3
+        global ganPositionArray4
+        global ganPositionArray5
+        global ganPositionArray6
+        global ganPositionArray7
+        global ganPositionArray8
+        global ganPositionArray9
+        global ganPositionArray10
+        global ganPositionArray11
+        global ganPositionArray12
+        global ganPositionArray13
+        global ganPositionArray14
+        global ganPositionArray15
+        global ganPositionArray16
+        global ganPositionArray17
+        global ganPositionArray18
+        global ganPositionArray19
+        global ganPositionArray20
+        #global actualGanJoint
 
-        if(globalGanCounter < 5866):
-            self.ganNodes["Node{0}".format(actualGanJoint)].setPos(ganPositionArray[globalGanCounter])
-            #self.ganNodes["Node{0}".format(actualGanJoint)].reparentTo(self.render)
-            #self.ganNodes["Node{0}".format(actualGanJoint)].show()
+        if(globalGanCounter < 279):
+            #self.ganNodes["Node{0}".format(actualGanJoint)].setPos(temp_ganPositionArray[globalGanCounter])
+            self.ganNodes["Node0"].setPos(ganPositionArray0[globalGanCounter])
+            self.ganNodes["Node1"].setPos(ganPositionArray1[globalGanCounter])
+            self.ganNodes["Node2"].setPos(ganPositionArray2[globalGanCounter])
+            self.ganNodes["Node3"].setPos(ganPositionArray3[globalGanCounter])
+            self.ganNodes["Node4"].setPos(ganPositionArray4[globalGanCounter])
+            self.ganNodes["Node5"].setPos(ganPositionArray5[globalGanCounter])
+            self.ganNodes["Node6"].setPos(ganPositionArray6[globalGanCounter])
+            self.ganNodes["Node7"].setPos(ganPositionArray7[globalGanCounter])
+            self.ganNodes["Node8"].setPos(ganPositionArray8[globalGanCounter])
+            self.ganNodes["Node9"].setPos(ganPositionArray9[globalGanCounter])
+            self.ganNodes["Node10"].setPos(ganPositionArray10[globalGanCounter])
+            self.ganNodes["Node11"].setPos(ganPositionArray11[globalGanCounter])
+            self.ganNodes["Node12"].setPos(ganPositionArray12[globalGanCounter])
+            self.ganNodes["Node13"].setPos(ganPositionArray13[globalGanCounter])
+            self.ganNodes["Node14"].setPos(ganPositionArray14[globalGanCounter])
+            self.ganNodes["Node15"].setPos(ganPositionArray15[globalGanCounter])
+            self.ganNodes["Node16"].setPos(ganPositionArray16[globalGanCounter])
+            self.ganNodes["Node17"].setPos(ganPositionArray17[globalGanCounter])
+            self.ganNodes["Node18"].setPos(ganPositionArray18[globalGanCounter])
+            self.ganNodes["Node19"].setPos(ganPositionArray19[globalGanCounter])
+            self.ganNodes["Node20"].setPos(ganPositionArray20[globalGanCounter])
+
+            self.ganNodes["Node0"].show()
+            self.ganNodes["Node1"].show()
+            self.ganNodes["Node2"].show()
+            self.ganNodes["Node3"].show()
+            self.ganNodes["Node4"].show()
+            self.ganNodes["Node5"].show()
+            self.ganNodes["Node6"].show()
+            self.ganNodes["Node7"].show()
+            self.ganNodes["Node8"].show()
+            self.ganNodes["Node9"].show()
+            self.ganNodes["Node10"].show()
+            self.ganNodes["Node11"].show()
+            self.ganNodes["Node12"].show()
+            self.ganNodes["Node13"].show()
+            self.ganNodes["Node14"].show()
+            self.ganNodes["Node15"].show()
+            self.ganNodes["Node16"].show()
+            self.ganNodes["Node17"].show()
+            self.ganNodes["Node18"].show()
+            self.ganNodes["Node19"].show()
+            self.ganNodes["Node20"].show()
+
+            print(self.ganNodes["Node0"].getPos())
+            print(self.ganNodes["Node8"].getPos())
+
             globalGanCounter += 1
-            actualGanJoint += 1
-            if(actualGanJoint == 21):
-                actualGanJoint = 0
 
         sleep(0.033)#Wait until next iteration
 
@@ -178,29 +264,95 @@ class ARtest(ShowBase):
         for i in range(21):
             cNode = CollisionNode("GanNode" + str(i))
             cNode.addSolid(CollisionSphere(0, 0, 0, 0.02))
-            self.ganNodes["Node{0}".format(i)] = cNode
-            self.ganNodes["Node{0}".format(actualGanJoint)].reparentTo(self.render)
-            self.ganNodes["Node{0}".format(actualGanJoint)].show()
+            nodePath = NodePath("NodePath{0}".format(i))
+            self.ganNodes["Node{0}".format(i)] = nodePath.attachNewNode(cNode)
+            self.ganNodes["Node{0}".format(i)].reparentTo(self.render)
+            #self.ganNodes["Node{0}".format(actualGanJoint)].show()
 
     def fillGanArray(self):
-        global ganPositionArray
+        #global temp_ganPositionArray
+        global ganPositionArray0
+        global ganPositionArray1
+        global ganPositionArray2
+        global ganPositionArray3
+        global ganPositionArray4
+        global ganPositionArray5
+        global ganPositionArray6
+        global ganPositionArray7
+        global ganPositionArray8
+        global ganPositionArray9
+        global ganPositionArray10
+        global ganPositionArray11
+        global ganPositionArray12
+        global ganPositionArray13
+        global ganPositionArray14
+        global ganPositionArray15
+        global ganPositionArray16
+        global ganPositionArray17
+        global ganPositionArray18
+        global ganPositionArray19
+        global ganPositionArray20
 
         contador = 0
         x = 0
         y = 0
         z = 0
 
-        for line in self.ganTxtLines:
-            if (contador % 5 == 2):
-                contador += 1
+        for i in range(len(self.ganTxtLines)):
+            if(i % 5 == 2):
                 continue
-            splitedString = line.split()
-            x = float(splitedString[0]) / 100
-            y = float(splitedString[2]) / 100
-            z = float(splitedString[1]) / 100
+            splitedString = self.ganTxtLines[i].split()
+            x = (float(splitedString[0]) / 200) * -1
+            y = float(splitedString[2]) / 50
+            z = float(splitedString[1]) / 200
             vector3f = LVecBase3f(x, y, z)
-            ganPositionArray.append(vector3f)
+            if (contador == 0):
+                ganPositionArray0.append(vector3f)
+            elif (contador == 1):
+                ganPositionArray1.append(vector3f)
+            elif (contador == 2):
+                ganPositionArray2.append(vector3f)
+            elif (contador == 3):
+                ganPositionArray3.append(vector3f)
+            elif (contador == 4):
+                ganPositionArray4.append(vector3f)
+            elif (contador == 5):
+                ganPositionArray5.append(vector3f)
+            elif (contador == 6):
+                ganPositionArray6.append(vector3f)
+            elif (contador == 7):
+                ganPositionArray7.append(vector3f)
+            elif (contador == 8):
+                ganPositionArray8.append(vector3f)
+            elif (contador == 9):
+                ganPositionArray9.append(vector3f)
+            elif (contador == 10):
+                ganPositionArray10.append(vector3f)
+            elif (contador == 11):
+                ganPositionArray11.append(vector3f)
+            elif (contador == 12):
+                ganPositionArray12.append(vector3f)
+            elif (contador == 13):
+                ganPositionArray13.append(vector3f)
+            elif (contador == 14):
+                ganPositionArray14.append(vector3f)
+            elif (contador == 15):
+                ganPositionArray15.append(vector3f)
+            elif (contador == 16):
+                ganPositionArray16.append(vector3f)
+            elif (contador == 17):
+                ganPositionArray17.append(vector3f)
+            elif (contador == 18):
+                ganPositionArray18.append(vector3f)
+            elif (contador == 19):
+                ganPositionArray19.append(vector3f)
+            elif (contador == 20):
+                ganPositionArray20.append(vector3f)
+
             contador += 1
+            if (contador == 21):
+                contador = 0
+
 
     def refreshCameraPosition(self, task):
         global globalCounter
