753a6a48aa2f92df349df27540ffd8e350fcc8c3
Vinicius Luiz Oliveira Pinheiro dos Santos <viniciuslops@hotmail.com>
2020-10-16 00:12:22 -0300

    Converter now works with text entry

--- Converter.py
+++ Converter.py
@@ -9,17 +9,28 @@ class Converter(object):
 
     #Set Slam start 3D point
     def setStart(self, firstPoint):
-        self.start = firstPoint
+        if (type(firstPoint) == str):
+            x, y, z = firstPoint.replace(',', ' ').split(" ")
+            Temp = [float(x),float(y),float(z)]
+            self.start = Temp
+        else:
+            self.start = firstPoint
 
     #Set Slam end 3D point
     def setEnd(self, lastPoint):
-        self.end = lastPoint
+        if (type(lastPoint) == str):
+            x, y, z = lastPoint.replace(',', ' ').split(" ")
+            Temp = [float(x),float(y),float(z)]
+            self.end = Temp
+        else:
+            self.end = lastPoint
 
     #Set Converted
     #def setConverted(self, value):
      #   self.Converted = value
 
     #Convert both points into the number equivalent to 1, meter in the case of our tests
+
     def Converter(self):
         #if self.start != None and self.end != None:
         #euclidean distance between 2 3d points
@@ -29,11 +40,19 @@ class Converter(object):
 
     #Distance factor default is 1 meter
     def ConvertSlamToWorld(self, xyz, distanceFactor = 1):
-        xTemp = (xyz[0]*distanceFactor)/self.Converted
-        yTemp = (xyz[1]*distanceFactor)/self.Converted
-        zTemp = (xyz[2]*distanceFactor)/self.Converted
-        xyzTemp = [xTemp, yTemp, zTemp]
-        return xyzTemp
+        if (type(xyz) == str):
+            x, y, z = xyz.replace(',', ' ').split(" ")
+            xTemp = (float(x)*distanceFactor)/self.Converted
+            yTemp = (float(y)*distanceFactor)/self.Converted
+            zTemp = (float(z)*distanceFactor)/self.Converted
+            xyzTemp = str(xTemp) + "," + str(yTemp) + "," + str(zTemp)
+            return xyzTemp
+        else:
+            xTemp = (xyz[0]*distanceFactor)/self.Converted
+            yTemp = (xyz[1]*distanceFactor)/self.Converted
+            zTemp = (xyz[2]*distanceFactor)/self.Converted
+            xyzTemp = [xTemp, yTemp, zTemp]
+            return xyzTemp
 
 if __name__ == '__main__':
     SlamConversor = Converter()
@@ -46,4 +65,16 @@ if __name__ == '__main__':
     print("Distance of Slam = ", SlamConversor.Converted)
     print("SlamVector = ", xyzSlam)
     xyzSlamTemp = SlamConversor.ConvertSlamToWorld(xyzSlam)
+    print("ConvertedSlamToWorld = ", xyzSlamTemp)
+
+    SlamConversor2 = Converter()
+    first2 = "10,0,0"
+    second2 = "0,0,0"
+    SlamConversor2.setStart(first2)
+    SlamConversor2.setEnd(second2)
+    SlamConversor2.Converter()
+    xyzSlam = "20,20,10"
+    print("Distance of Slam = ", SlamConversor2.Converted)
+    print("SlamVector = ", xyzSlam)
+    xyzSlamTemp = SlamConversor2.ConvertSlamToWorld(xyzSlam)
     print("ConvertedSlamToWorld = ", xyzSlamTemp)
